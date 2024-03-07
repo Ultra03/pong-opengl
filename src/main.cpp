b25693d9d8a9d166f25ec7fa964fdf94449c5581
Lucas <cog.lucas@gmail.com>
2024-03-07 10:57:01 -0800

    GLFW start

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Test", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "failed to create glfw window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
